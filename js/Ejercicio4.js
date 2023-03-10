05cffa3cf6fc1abc9400a7711a215f2478e61583
cristianq3 <cristianq3@gmail.com>
2023-03-10 17:38:12 -0300

    Finalizado Ejercicio 4

--- js/Ejercicio4.js
+++ js/Ejercicio4.js
@@ -1,3 +1,14 @@
-//4- Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con “cancelar” deberá indicarse la suma total de los números introducidos.
+//4- Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con “cancelar” deberá indicarse la suma total de los ido números introducs+zz¿'x<p'pxd.
+let suma = 0;
 
+do {
+  let numero = parseInt(prompt("Ingrese un número"));
 
+  if (!isNaN(numero) === true) {
+    suma = parseInt(suma) + numero;
+  } else {
+    alert("Ingrese un Número válido");
+  }
+} while (confirm("Quiere agregar otro número?"));
+
+document.write(`<p>La Suma es  <strong> ${suma}</strong></p> `);

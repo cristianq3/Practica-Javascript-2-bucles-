37e715c6d607a25b67f5db4a0b2fcdb7e0f456eb
cristianq3 <cristianq3@gmail.com>
2023-03-12 04:34:29 -0300

    Finalizado Ejercicio 17

---
+++ js/Ejercicio17.js
@@ -0,0 +1,21 @@
+// Realiza un script que muestre la posición de la primera vocal de un texto introducido por teclado.
+
+const frase = prompt("Ingrese un texto y se mostrará la primera vocal");
+
+for (let i = 0; i < frase.length; i++) {
+  if (
+    frase.charAt(i) === "a" ||
+    frase.charAt(i) === "e" ||
+    frase.charAt(i) === "i" ||
+    frase.charAt(i) === "o" ||
+    frase.charAt(i) === "u" ||
+    frase.charAt(i) === "A" ||
+    frase.charAt(i) === "E" ||
+    frase.charAt(i) === "I" ||
+    frase.charAt(i) === "O" ||
+    frase.charAt(i) === "U"
+  ) {
+    document.write(frase.charAt(i));
+    break;
+  }
+}
